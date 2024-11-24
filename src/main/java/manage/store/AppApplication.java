e8e43542c2d90c30f9b72e14b5870910c55a413a
hs.lee0130 <chickenman10@naver.com>
2024-11-24 13:41:19 +0900

    [init] - 멀티 모듈화로 인한 패키지 구조 변경 - 불필요 코드 삭제

--- src/main/java/manage/store/AppApplication.java
+++ src/main/java/manage/store/AppApplication.java
@@ -1,4 +1,4 @@
-package manage.store.app;
+package manage.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,12 +6,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(scanBasePackages = {"manage.store"})
 public class AppApplication {
-
 	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(AppApplication.class, args);
-		for (String beanName : ctx.getBeanDefinitionNames()) {
-			System.out.println(beanName);
-		}
+		SpringApplication.run(AppApplication.class, args);
 	}
-
 }
